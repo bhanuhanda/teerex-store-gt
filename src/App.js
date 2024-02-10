b32238a38b5e391867fc971cb839e8ca49c3d32e
Bhanu Handa <bhanuhanda98@gmail.com>
2024-02-10 22:03:22 +0530

    setup react router & routing with gh pages

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <h1>TeeRex Store</h1>;
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Products />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+], {
+  basename: "/teerex-store-gt",
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
